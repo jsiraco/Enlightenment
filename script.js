97e45f3c894c868c0ec51219f2c842911eaf07ba
Jas <ragnarock.josef@gmail.com>
2021-07-28 20:32:17 -0400

    Updated JS for images

---
+++ script.js
@@ -0,0 +1,5 @@
+
+
+//All public domain metmuseum images
+//only public domain images can have image URL accessed
+//https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&q=isPublicDomain
